c7e31a74c88084adfa0d2a2285696233d302c367
laubonghaudoi <laubonghaudoi@icloud.com>
2020-12-18 16:30:10 -0800

    Fix non-null type assertion

--- imports/api/tasksPublications.ts
+++ imports/api/tasksPublications.ts
@@ -2,5 +2,5 @@ import { Meteor } from "meteor/meteor";
 import { TasksCollection } from "/imports/db/TasksCollection";
 
 Meteor.publish("tasks", function publishTasks() {
-  return TasksCollection.find({ userId: this.userId || undefined });
+  return TasksCollection.find({ userId: this.userId! });
 });
